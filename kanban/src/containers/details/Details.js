e58832d7408df9ae24cc0c4ba600ba87ea19b481
Neha-rajbhar <nr470245@gmail.com>
2023-05-19 14:20:04 +0530

    title

--- kanban/src/containers/details/Details.js
+++ kanban/src/containers/details/Details.js
@@ -9,136 +9,158 @@ import Description from "../../components/description/Description";
 import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { ListData, dialogBox } from "../../recoil/atom";
-import { Dialog, DialogContent,Backdrop } from "@mui/material";
+import { Dialog, DialogContent, Backdrop, Button } from "@mui/material";
 
 import { useRecoilValue } from "recoil";
 
-
-
 function Details() {
+  const [initialTitle, setInitialTitle] = useState("");
+
+  const [newTitle, setNewTitle] = useState("");
+
   const [isDialog, setIsDialog] = useRecoilState(dialogBox);
   const { boardId, cardId } = useParams();
-  // console.log(boardId,)
-  // console.log(cardId)
-  let data;
   const [showTitle, setShowTitle] = useState(true);
-  const [input, setInput] = useState(data);
-  // const = useRecoilValue(ListData);
   const [globalListData, setGlobalListData] = useRecoilState(ListData);
   const navigate = useNavigate();
 
+  const [currentCard, setCurrentCard] = useState(null);
 
-  const [selectedTask, setSelectedTask] = useState(null);
-  const [currentBoard, setCurrentBoard] = useState("");
-  const [currentCard, setCurrentCard] = useState("");
-
-  const handleCardClick = (task) => {
-    setSelectedTask(task);
-  };
   useEffect(() => {
-    let tempListData = [...globalListData];
-    let index = tempListData.findIndex((ele) => ele.id === boardId);
-    let currentBoard = { ...tempListData[index] };
-    // currentBoard.boardName = listTitle;
-    setCurrentBoard(currentBoard);
-
-    let tempBoard = { ...currentBoard };
-    let tempCardData = [...tempBoard.cards];
-    let cardIndex = tempCardData.findIndex((ele) => ele.cardID === cardId);
-    // console.log(cardIndex);
-    let currentCard = { ...tempCardData[cardIndex] };
-    // console.log("bOARD",currentBoard);
-    // console.log("CARD",currentCard)
-    setCurrentCard(currentCard);
-    data = currentCard;
-  }, []);
+    const tempListData = [...globalListData];
+    const index = tempListData.findIndex((ele) => ele.id === boardId);
+    const currentBoard = tempListData[index];
+    if (currentBoard && currentBoard.cards) {
+      setCurrentCard(currentBoard.cards.find((card) => card.cardID === cardId));
+      setInitialTitle(
+        currentBoard.cards.find((card) => card.cardID === cardId)?.cardTitle ||
+          ""
+      );
+    }
+  }, [globalListData, boardId, cardId]);
 
   function handleTitle() {
     setShowTitle(!showTitle);
   }
-  // console.log(tasks.boardName);
-  function handleClose(){
-    setIsDialog()
 
-  const {cardId, boardId}=useParams()
-  // console.log(cardId)
-
-  const allLists=useRecoilValue(ListData)
-  const [requiredList]=allLists.filter(item=>item.id==boardId)
-  const [requiredCard]=requiredList.cards.filter(card=>card.cardID==cardId)
-
-
-  function handleInput(e) {
-    setInput(e.target.value);
-
+  function handleClose() {
+    setIsDialog(false);
   }
+
+  const allLists = useRecoilValue(ListData);
+  const [requiredList] = allLists.filter((item) => item.id === boardId);
+  const [requiredCard] =
+    requiredList && requiredList.cards
+      ? requiredList.cards.filter((card) => card.cardID === cardId)
+      : [];
+
+  const handleInput = (event) => {
+    setNewTitle(event.target.value);
+  };
+
+  const updateTitle = () => {
+    const previousData = [...globalListData];
+
+    const updatedData = previousData.map((list, listInd) => {
+      if (list.id === boardId) {
+        const updatedCards = list.cards.map((card, cardIndex) => {
+          if (card.cardID === cardId) {
+            return { ...card, cardTitle: newTitle };
+          }
+          return card;
+        });
+        return { ...list, cards: updatedCards };
+      }
+      return list;
+    });
+
+    setGlobalListData(updatedData);
+    console.log(updatedData);
+    localStorage.setItem("board", JSON.stringify(updatedData));
+    setShowTitle(!showTitle);
+  };
+
   return (
     <>
       <Backdrop open={isDialog} onClick={handleClose} />
-     <Dialog
-          open={isDialog}
-          PaperProps={{
-            sx: {
-              maxWidth: "40vw",
-              width: 800,
-              minHeight: "80vh",
-              backgroundColor: "whitesmoke",
-              // marginBottom: "3rem",
-            },
-          }}
-          // BackdropComponent={Backdrop}
-          // BackdropProps={{ invisible: true }}
-        >
-           <DialogContent>
-     <div className={style.main}>
-      <div className={style.windows}>
-        <div className={style.textAreaSection}>
-          <span className={style.laptop}>
-            <Icons icon={<FaLaptop />} />
-          </span>
-          <span className={style.textArea}>
-            {showTitle ? (
-              <>
-                <p
-                  onClick={handleTitle}
+      <Dialog
+        open={isDialog}
+        PaperProps={{
+          sx: {
+            maxWidth: "40vw",
+            width: 800,
+            minHeight: "80vh",
+            backgroundColor: "whitesmoke",
+          },
+        }}
+      >
+        <DialogContent>
+          <div className={style.titleWrapper}>
+            <div className={style.laptop}>
+              <FaLaptop />
+            </div>
+            <div className={style.title}>
+              {showTitle ? (
+                <h3>{initialTitle}</h3>
+              ) : (
+                <span className={style.textArea}>
+                  <input
+                    key={currentCard && currentCard.cardID}
+                    type="text"
+                    value={newTitle}
+                    onChange={handleInput}
+                    style={{ width: "90%", border: "none", height: "30px" }}
+                  />
+                </span>
+              )}
+              <div>
+                <Button
+                  variant="contained"
                   style={{
-                    width: "90%",
-                    border: "none",
+                    backgroundColor: "#00a82d",
+                    color: "white",
                     height: "30px",
-                    backgroundColor: "white",
-                    fontSize: "20px",
+                    marginRight: "5px",
+                    marginTop: "10px",
+                    fontSize: "12px",
                   }}
+                  onClick={handleTitle}
                 >
-                  {currentCard.cardTitle}
-                </p>
-              </>
-            ) : (
-              <>
-                <input
-                  key={currentCard.cardID}
-                  type="text"
-                  value={input}
-                  style={{ width: "90%", border: "none", height: "30px" }}
-                />
-              </>
-            )}
-
-            <p className={style.p}>in list {currentBoard.boardName}</p>
-          </span>
-          <span className={style.cross}>
-            <Icons icon={<RxCross2 onClick={() => navigate("/")} />} />
-          </span>
-        </div>
-        <div>
-          <Description />
-          <Activity cardId={cardId} boardId={boardId} cardActivityLog={requiredCard.activityLog}/>
-        </div>
-      </div>
-    </div>
-    </DialogContent>
-    </Dialog>
+                  {showTitle ? "Update Title" : "Cancel"}
+                </Button>
+                {!showTitle && (
+                  <Button
+                    variant="contained"
+                    style={{
+                      backgroundColor: "#00a82d",
+                      color: "white",
+                      height: "30px",
+                      marginTop: "10px",
+                      fontSize: "12px",
+                    }}
+                    onClick={updateTitle}
+                  >
+                    Save
+                  </Button>
+                )}
+              </div>
+            </div>
+            <span className={style.cross}>
+              <Icons icon={<RxCross2 onClick={() => navigate("/")} />} />
+            </span>
+          </div>
+          <div>
+            <Description />
+            <Activity
+              cardId={cardId}
+              boardId={boardId}
+              cardActivityLog={requiredCard.activityLog}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
-   
   );
 }
+
 export default Details;
