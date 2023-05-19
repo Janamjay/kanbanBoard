8db0c994679d994d568ad2a5b4b438a14a58a417
Janammjay <janammjayk6@gmail.com>
2023-05-19 10:33:43 +0530

    completed

--- kanban/src/containers/details/Details.js
+++ kanban/src/containers/details/Details.js
@@ -7,26 +7,18 @@ import Activity from "../../components/activity/Activity";
 import Description from "../../components/description/Description";
 
 import { useNavigate, useParams } from "react-router-dom";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { ListData, dialogBox } from "../../recoil/atom";
-import { Dialog, DialogContent,Backdrop } from "@mui/material";
-
-import { useRecoilValue } from "recoil";
-
-
+import { Dialog, DialogContent, Backdrop } from "@mui/material";
 
 export default function Details() {
   const [isDialog, setIsDialog] = useRecoilState(dialogBox);
   const { boardId, cardId } = useParams();
-  // console.log(boardId,)
-  // console.log(cardId)
   let data;
   const [showTitle, setShowTitle] = useState(true);
   const [input, setInput] = useState(data);
-  // const = useRecoilValue(ListData);
   const [globalListData, setGlobalListData] = useRecoilState(ListData);
   const navigate = useNavigate();
-
 
   const [selectedTask, setSelectedTask] = useState(null);
   const [currentBoard, setCurrentBoard] = useState("");
@@ -35,20 +27,17 @@ export default function Details() {
   const handleCardClick = (task) => {
     setSelectedTask(task);
   };
+
   useEffect(() => {
     let tempListData = [...globalListData];
     let index = tempListData.findIndex((ele) => ele.id === boardId);
     let currentBoard = { ...tempListData[index] };
-    // currentBoard.boardName = listTitle;
     setCurrentBoard(currentBoard);
 
     let tempBoard = { ...currentBoard };
-    let tempCardData = [...tempBoard.cards];
+    let tempCardData = tempBoard.cards || []; // Ensure tempBoard.cards is an iterable array
     let cardIndex = tempCardData.findIndex((ele) => ele.cardID === cardId);
-    // console.log(cardIndex);
     let currentCard = { ...tempCardData[cardIndex] };
-    // console.log("bOARD",currentBoard);
-    // console.log("CARD",currentCard)
     setCurrentCard(currentCard);
     data = currentCard;
   }, []);
@@ -56,89 +45,91 @@ export default function Details() {
   function handleTitle() {
     setShowTitle(!showTitle);
   }
-  // console.log(tasks.boardName);
-  function handleClose(){
-    setIsDialog()
+
+  function handleClose() {
+    setIsDialog(false);
   }
-  
-  // console.log(cardId)
 
-  const allLists=useRecoilValue(ListData)
-  const [requiredList]=allLists.filter(item=>item.id==boardId)
-  const [requiredCard]=requiredList.cards.filter(card=>card.cardID==cardId)
-
+  const allLists = useRecoilValue(ListData);
+  const requiredList = allLists.find((item) => item.id === boardId);
+  const requiredCard = requiredList?.cards.find((card) => card.cardID === cardId);
+  const cardActivityLog = requiredCard?.activityLog || [];
 
   function handleInput(e) {
     setInput(e.target.value);
-
   }
+
   return (
     <>
       <Backdrop open={isDialog} onClick={handleClose} />
-     <Dialog
-          open={isDialog}
-          PaperProps={{
-            sx: {
-              maxWidth: "40vw",
-              width: 800,
-              minHeight: "80vh",
-              backgroundColor: "whitesmoke",
-              // marginBottom: "3rem",
-            },
-          }}
-          // BackdropComponent={Backdrop}
-          // BackdropProps={{ invisible: true }}
-        >
-           <DialogContent>
-     <div className={style.main}>
-      <div className={style.windows}>
-        <div className={style.textAreaSection}>
-          <span className={style.laptop}>
-            <Icons icon={<FaLaptop />} />
-          </span>
-          <span className={style.textArea}>
-            {showTitle ? (
-              <>
-                <p
-                  onClick={handleTitle}
-                  style={{
-                    width: "90%",
-                    border: "none",
-                    height: "30px",
-                    backgroundColor: "white",
-                    fontSize: "20px",
-                  }}
-                >
-                  {currentCard.cardTitle}
-                </p>
-              </>
-            ) : (
-              <>
-                <input
-                  key={currentCard.cardID}
-                  type="text"
-                  value={input}
-                  style={{ width: "90%", border: "none", height: "30px" }}
+      <Dialog
+        open={isDialog}
+        PaperProps={{
+          sx: {
+            maxWidth: "40vw",
+            width: 800,
+            minHeight: "80vh",
+            backgroundColor: "whitesmoke",
+          },
+        }}
+      >
+        <DialogContent>
+          <div className={style.main}>
+            <div className={style.windows}>
+              <div className={style.textAreaSection}>
+                <span className={style.laptop}>
+                  <Icons icon={<FaLaptop />} />
+                </span>
+                <span className={style.textArea}>
+                  {showTitle ? (
+                    <>
+                      <p
+                        onClick={handleTitle}
+                        style={{
+                          width: "90%",
+                          border: "none",
+                          height: "30px",
+                          backgroundColor: "white",
+                          fontSize: "20px",
+                        }}
+                      >
+                        {currentCard.cardTitle}
+                      </p>
+                    </>
+                  ) : (
+                    <>
+                      <input
+                        key={currentCard.cardID}
+                        type="text"
+                        value={input}
+                        style={{
+                          width: "90%",
+                          border: "none",
+                          height: "30px",
+                        }}
+                      />
+                    </>
+                  )}
+                  <p className={style.p}>in list {currentBoard.boardName}</p>
+                </span>
+                <span className={style.cross}>
+                  <Icons
+                    icon={<RxCross2 onClick={() => navigate("/")} />}
+                  />
+                </span>
+              </div>
+              <div>
+                <Description />
+                <Activity
+                  cardId={cardId}
+                  boardId={boardId}
+                  cardActivityLog={cardActivityLog}
                 />
-              </>
-            )}
-
-            <p className={style.p}>in list {currentBoard.boardName}</p>
-          </span>
-          <span className={style.cross}>
-            <Icons icon={<RxCross2 onClick={() => navigate("/")} />} />
-          </span>
-        </div>
-        <div>
-          <Description />
-          <Activity cardId={cardId} boardId={boardId} cardActivityLog={requiredCard.activityLog}/>
-        </div>
-      </div>
-    </div>
-    </DialogContent>
-    </Dialog>
+              </div>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
-   
   );
-            }
-
+}
