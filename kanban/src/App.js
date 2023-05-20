8a6b958d4444236c1ecf2e624c7a408d4cb17698
Janammjay <janammjayk6@gmail.com>
2023-05-20 15:27:25 +0530

    time added

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
 import Home from "./home/Home";
-import  Details from './containers/details/Details'
+import Details from "./containers/details/Details";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={"/"} element={<Home />}/>
-
-          <Route path={"/:boardId/:cardId"} element={<Details />} />   
-
-          <Route path={"/:boardId/:cardId/:cardTitle"} element={<Details />} />   
-
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/:boardId/:cardId/:cardTitle"} element={<Details />} />
       </Routes>
     </div>
   );
